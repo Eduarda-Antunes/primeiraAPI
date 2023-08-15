399f968e8533f3f9f2248a5da38db9e879c69427
Eduarda Oliveira <duda.21antunes@gmail.com>
2023-08-15 12:22:35 -0300

    primeiraapi

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express'); //esse pacote quer utilizar o server
+
+const app = express(); //cria uma instância do express
+
+app.listen(3000, () => console.log('Running at port 3000')) //qual porta o servidor vai ouvir + eurofunction para testar
+
